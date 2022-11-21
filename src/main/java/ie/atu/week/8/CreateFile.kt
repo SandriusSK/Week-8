b4d7b60cfe99f36ffb8776cc39eab7b20a0a0a6f
g00402163 <G00402163@atu.ie>
2022-11-21 09:28:24 +0000

    First File

---
+++ src/main/java/ie/atu/week/8/CreateFile.kt
@@ -0,0 +1,11 @@
+package ie.atu.week.`8`;
+
+import java.io.*;
+
+ public class CreateFile {
+     public static void main(String[] args) {
+         {
+             File myFile = new File
+         }
+     }
+}
