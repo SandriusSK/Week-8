1b12f0cf9a91a5579b085f8c34e26a6b9142bdbc
g00402163 <G00402163@atu.ie>
2022-11-21 09:56:38 +0000

    Adding An Existing File

--- src/main/java/ie/atu/week8/CreateFile.java
+++ src/main/java/ie/atu/week8/CreateFile.java
@@ -1,6 +1,7 @@
 package ie.atu.week8;
 
 import java.io.*;
+import java.nio.file.Path;
 
 public class CreateFile {
     public static void main(String[] args) {
@@ -8,6 +9,17 @@ public class CreateFile {
         {
             File myFile = new File("MyFile.txt");
             System.out.println("My File is located at " + myFile.getAbsolutePath());
+            String content = "Details to write";
+            try
+            {
+                FileWriter myWriter = new FileWriter(myFile,true);
+                myWriter.write("This is my first line");
+                myWriter.close();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
         }
 
     }
